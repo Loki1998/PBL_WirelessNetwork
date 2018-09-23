e9d7280962fe60303398fcbb5c6b24e24412455b
Loki1998 <1663176432@qq.com>
2018-09-23 18:53:40 +0800

    First version of Tx.py

--- Tx.py
+++ Tx.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python
 # coding=utf-8
+import socket
+
+host="192.168.100.xx"
+port=8080
+
+while True:
+    serversock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    serversock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    serversock.bind((host,port))
+    server.listen(10)
+
+    print 'Waitting for connections ...'
+    clientsock.client_address=serversock.accept()
+
+    recvmsg=clientsock.recv(1024)
+    print 'Received -> %s'%(recvmsg)
+
+    print 'Type command for Rx'
+    s_msg=raw_input()
+
+    clientsock.sendall(s_msg)
+clientsock.close()
 
